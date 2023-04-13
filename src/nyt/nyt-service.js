0cfce9cf0587cc1b86aa72dab00eab9296b01603
Jesie F <jesiefu11@gmail.com>
2023-04-13 15:32:02 -0400

    proj edits

--- src/nyt/nyt-service.js
+++ src/nyt/nyt-service.js
@@ -3,6 +3,9 @@ const NYT_API = "https://api.nytimes.com/svc/mostpopular/v2/viewed/{period}.json
 const NYT_KEY = process.env.REACT_APP_NYT_KEY;
 
 export const textSearch = async (query)=> {
-    const response = await axios.get(`${NYT_API}/shared/${query}.json&apikey=${NYT_KEY}`);
-    return response.data;
+    //const response = await axios.get(`${NYT_API}/shared/${query}.json&apikey=${NYT_KEY}`);
+    console.log(NYT_KEY);
+    const response = await axios.get(`https://api.nytimes.com/svc/mostpopular/v2/shared/${query}.json?&api-key=${NYT_KEY}`);
+
+        return response.data;
 }
