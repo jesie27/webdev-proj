54537228d69b3588307a92f2d61cb7887b1c0fe0
Jesie F <jesiefu11@gmail.com>
2023-04-16 11:35:29 -0400

    proj edits

--- src/news-site/home/index.js
+++ src/news-site/home/index.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {searchMostViewed} from "../../nyt/nyt-service";
 const HomeComponent = () => {
+    const [search, setSearch] = useState("");
+    const [results, setResults] = useState([]);
+    const searchNyt = async() => {
+        const response = await searchMostViewed(1);
+        setResults(response);
+        console.log(response);
+        ;
+    }
+    const homeResults = searchNyt();
+    console.log(results)
+
 
     return (
         <div>
             <h1>Home</h1>
+            <h2>homeResults</h2>
         </div>
     );
 }
--- src/nyt/nyt-service.js
+++ src/nyt/nyt-service.js
@@ -8,8 +8,13 @@ export const searchMostShared = async (query)=> {
     return response.data;
 }
 
+export const searchMostViewed = async (query)=> {
+    const response = await axios.get(`https://api.nytimes.com/svc/mostpopular/v2/viewed/${query}.json?&api-key=${NYT_KEY}`);
+    console.log(response);
+    return response.data;
+}
+
 export const generalSearch = async (query)=> {
-    //const response = await axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch${query}.json?&api-key=${NYT_KEY}`);
     const response = await axios.get(`https://api.nytimes.com/svc/search/v2//articlesearch.json?q=${query}&fq=source:("The New York Times")
 &api-key=${NYT_KEY}`);
 
@@ -21,3 +26,4 @@ export const getArticle = async(articleID) => {
     const response = await axios.get(`https://api.nytimes.com/svc/mostpopular/v2/shared/${articleID}.json?&api-key=${NYT_KEY}`);
 return response.data.id;
 }
+
