831d26da9c0ba146791717efdba9bc1a3aa2a540
Jesie F <jesiefu11@gmail.com>
2023-04-16 12:52:20 -0400

    proj edits

--- src/news-site/home/index.js
+++ src/news-site/home/index.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {searchMostViewed} from "../../nyt/nyt-service";
+import {Link} from "react-router-dom";
 const HomeComponent = () => {
     const [search, setSearch] = useState("");
     const [results, setResults] = useState([]);
@@ -14,6 +15,20 @@ const HomeComponent = () => {
     return (
         <div>
             <h1>Home</h1>
+            <ul className="list-group mt-3">
+            {results.results && results.results.map((results) => (
+                <li className="list-group-item"  key={results.id}>
+                    <div className="mt-2">{results.published_date}</div>
+                    <Link to={results.url}><h3>{results.title}</h3></Link>
+                    <div>{results.byline}</div>
+                    <Link to={`/nyt/popular-article/${results.id}`}>
+                        <div className="mb-2">{results.abstract}</div>
+                    </Link>
+                    <img src=""/>
+                </li>))}
+        </ul>
+
+
             <h2></h2>
             <pre>{JSON.stringify(results, null, 2)}</pre>
             <h2>{}</h2>
